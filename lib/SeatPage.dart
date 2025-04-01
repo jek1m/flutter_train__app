8cb512fe2c0fa47ce881917087c5ba789fdc8cf5
jek1m <jieunkim7912@gmail.com>
2025-04-02 02:08:45 +0900

    feat : SeatPage 아이콘 구현

--- lib/SeatPage.dart
+++ lib/SeatPage.dart
@@ -12,9 +12,61 @@ class Seatpage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: const Text('좌석 선택')),
-      body: Center(
-        child: Text('출발역: $startStation\n도착역: $endStation'),
-      ),
+      body: Column(
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              station(startStation),
+              const SizedBox(width: 50),
+              const Icon(
+                Icons.arrow_circle_right_outlined,
+                size: 40,
+              ),
+              const SizedBox(width: 50),
+              station(endStation),
+            ]
+          ),
+          const SizedBox(height: 30),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              selectsquare(Colors.purple),
+              const SizedBox(width: 8),
+              Text("선택됨"),
+              const SizedBox(width: 30),
+              selectsquare(Colors.grey[350]!),
+              const SizedBox(width: 8),
+              Text("선택안됨"),
+            ]
+
+          )
+
+        ]
+        
+      )
     );
+  }
+
+  Container selectsquare(Color color) {
+    return Container(
+              width: 30,
+              height: 30,
+              decoration: BoxDecoration(
+                color: color,
+                borderRadius: BorderRadius.circular(10),
+              ),
+            );
+  }
+
+  Text station(String text) {
+    return Text(
+              text,
+              style: const TextStyle(
+                fontSize: 30,
+                color: Colors.purple, 
+                fontWeight: FontWeight.bold 
+              ),
+            );
   }
 }
