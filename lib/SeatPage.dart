7f79cf9c60f8d461fceb0ea3ab07d9feff3daa5d
jek1m <jieunkim7912@gmail.com>
2025-04-02 02:34:38 +0900

    feat : SeatPage 좌석 선택 위젯 구현

--- lib/SeatPage.dart
+++ lib/SeatPage.dart
@@ -33,20 +33,70 @@ class Seatpage extends StatelessWidget {
             children: [
               selectsquare(Colors.purple),
               const SizedBox(width: 8),
-              Text("선택됨"),
+              const Text("선택됨"),
               const SizedBox(width: 30),
               selectsquare(Colors.grey[350]!),
               const SizedBox(width: 8),
-              Text("선택안됨"),
+              const Text("선택안됨"),
             ]
+          ),
+          const SizedBox(height: 30),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              ABCD("A"),
+              const SizedBox(width: 30),
+              ABCD("B"),
+              const SizedBox(width: 105),
+              ABCD("C"),
+              const SizedBox(width: 30),
+              ABCD("D"),
+            ]
+          ),
+          const SizedBox(height: 10),
+          Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+            for (var i = 1; i <= 20; i++) ...[  // 여러 Row를 세로로 나열
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    seatsquare(),
+                    const SizedBox(width: 2.5),
+                    seatsquare(),
+                    const SizedBox(width: 32.5),
+                    Text("$i", style: TextStyle(fontSize: 20)),
+                    const SizedBox(width: 32.5),
+                    seatsquare(),
+                    const SizedBox(width: 2.5),
+                    seatsquare(),
+                  ],
+                ),
+                const SizedBox(height: 10),  // Row 사이 간격 추가
+              ],
 
+            ],
           )
+
 
         ]
         
       )
     );
   }
+
+  Container seatsquare() {
+    return Container(
+              width: 40,
+              height: 40,
+              decoration: BoxDecoration(
+                color: Colors.grey[350],
+                borderRadius: BorderRadius.circular(10),
+              ),
+            );
+  }
+
+  Text ABCD(String text) => Text(text,style: TextStyle(fontSize: 20),);
 
   Container selectsquare(Color color) {
     return Container(
